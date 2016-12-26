55f7e5ee9c97963650a29b8888f93d329ef80353
stiig <vasiliyrps2@gmail.com>
2016-12-27 03:45:53 +0500

    install rspec and add generators

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -51,4 +51,9 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+
+  config.generators do |g|
+    g.test_framework  :rspec
+    g.integration_tool :rspec
+  end
 end
