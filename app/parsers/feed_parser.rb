652fe18c480657b0c262aca2ed68ea7da9ac020c
stiig <vasiliyrps2@gmail.com>
2017-01-12 23:47:04 +0500

    add spec for feed_parser and improve feed_parser

--- app/parsers/feed_parser.rb
+++ app/parsers/feed_parser.rb
@@ -7,22 +7,40 @@ class FeedParser
   end
 
   def perform
-    doc = open(@url)
-    @parse_page = RSS::Parser.parse(doc, false)
-    item_container if @parse_page
+    doc = open(@url, read_timeout: 5, open_timeout: 3).read
+    doc.gsub!('encoding="windows-1251"', 'encoding="UTF-8"')
+    @parsed_feed = RSS::Parser.parse(doc, false)
+    item_container if @parsed_feed
   end
 
   private
 
   def item_container
-    items = []
-    @parse_page.items.each do |item|
-      items << {
+    case @parsed_feed.feed_type
+    when 'atom'
+      parse_atom(@parsed_feed)
+    when 'rss'
+      parse_rss(@parsed_feed)
+    end
+  end
+
+  def parse_atom(feed)
+    feed.items.collect do |item|
+      {
+        date: Time.zone.parse(item.published.to_s),
+        content: item.title.content,
+        link: item.link.href
+      }
+    end
+  end
+
+  def parse_rss(feed)
+    feed.items.collect do |item|
+      {
         date: Time.zone.parse(item.pubDate.to_s),
         content: item.title,
         link: item.link
       }
     end
-    items
   end
 end
--- spec/parsers/feed_parser_spec.rb
+++ spec/parsers/feed_parser_spec.rb
@@ -1,14 +1,74 @@
 # frozen_string_literal: true
 describe FeedParser do
-  before(:each) do
-    stub_request(:get, 'http://example.com/rss').
-      with(headers: { Accept: '*/*', 'Accept-Encoding': 'gzip;q=1.0,deflate;q=0.6,identity;q=0.3', 'User-Agent': 'Ruby' }).
-      to_return(status: 200, body: '', headers: {})
-  end
+  let(:uri) { 'http://example.com/rss' }
 
   context '#perform' do
     it 'should return nil if body is empty' do
-      expect(described_class.new('http://example.com/rss').perform).to be nil
+      stub_request(:get, uri)
+
+      expect(described_class.new(uri).perform).to be nil
+    end
+
+    it 'should raise Net::OpenTimeout error if connection time out' do
+      stub_request(:get, uri).to_timeout
+
+      expect { described_class.new(uri).perform }.to raise_error Net::OpenTimeout
+    end
+
+    it 'should return nil if responce invalid' do
+      stub_request(:get, uri).to_return(body: 'invalid body')
+
+      expect(described_class.new(uri).perform).to be nil
+    end
+
+    context 'when rss link is right' do
+      # FIXME: fix time zone
+      let(:time_now) { Time.current.to_time }
+
+      let(:atom_body) do
+        RSS::Maker.make('atom') do |maker|
+          maker.channel.author = 'Someone'
+          maker.channel.updated = time_now
+          maker.channel.about = uri
+          maker.channel.title = 'Example Feed'
+
+          maker.items.new_item do |item|
+            item.link = "#{uri}/example"
+            item.title = 'Ruby 1.9.2-p136 is released'
+            item.published = time_now
+            item.updated = time_now
+          end
+        end
+      end
+
+      let(:rss_body) do
+        RSS::Maker.make('2.0') do |maker|
+          maker.channel.author = 'Someone'
+          maker.channel.updated = time_now
+          maker.channel.link = uri
+          maker.channel.description = 'Some description'
+          maker.channel.title = 'Example Feed'
+
+          maker.items.new_item do |item|
+            item.link = "#{uri}/example"
+            item.title = 'Ruby 1.9.2-p136 is released'
+            item.pubDate = time_now
+            item.updated = time_now
+          end
+        end
+      end
+
+      it 'should return array of hashes when atom body' do
+        stub_request(:get, uri).to_return(body: atom_body.to_s)
+
+        expect(described_class.new(uri).perform).to be_kind_of Array
+      end
+
+      it 'should return array of hashes when rss body' do
+        stub_request(:get, uri).to_return(body: rss_body.to_s)
+
+        expect(described_class.new(uri).perform).to be_kind_of Array
+      end
     end
   end
 end
