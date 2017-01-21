6f2f64d9b5c43cdbb0d114115853246dafcb352a
stiig <vasiliyrps2@gmail.com>
2017-01-21 23:53:17 +0500

    add kaminari to pagination

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  concern :paginatable do
+    get '(page/:page)', action: :index, on: :collection, as: ''
+  end
   root 'main#index'
+  resources :main, only: :index, concerns: :paginatable
 end
