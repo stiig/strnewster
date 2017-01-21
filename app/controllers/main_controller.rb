312c4a0f80a1bc228a7f9277e4cdb7e124e9dc26
stiig <vasiliyrps2@gmail.com>
2017-01-21 23:51:52 +0500

    improve main controller
    receive actual data

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,4 +1,6 @@
 # frozen_string_literal: true
 class MainController < ApplicationController
-  def index; end
+  def index
+    @parsed_articles = ParsedArticle.includes(:feed_source).order(date: :desc).page(params[:page]).per(12)
+  end
 end
