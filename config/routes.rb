4690a4157832cd443bfa92ccd932788648e9dd67
stiig <vasiliyrps2@gmail.com>
2017-01-25 22:22:15 +0500

    empty path for main controller, bundle update

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   concern :paginatable do
     get '(page/:page)', action: :index, on: :collection, as: ''
   end
+
   root 'main#index'
-  resources :main, only: :index, concerns: :paginatable
+  resources :main, only: :index, concerns: :paginatable, path: ''
 end
