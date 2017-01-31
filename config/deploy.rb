e42b4a104c94dbc66d599c00a8ab74672f3f9a2c
stiig <vasiliyrps2@gmail.com>
2017-01-31 23:31:56 +0500

    remover pty from deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -20,7 +20,6 @@ set :deploy_to, "/home/#{fetch(:user)}/apps/#{fetch(:application)}"
 # set :format_options, command_output: true, log_file: "log/capistrano.log", color: :auto, truncate: :auto
 
 # Default value for :pty is false
-set :pty, true
 
 # Default value for :linked_files is []
 append :linked_files, 'config/database.yml', 'config/secrets.yml'
