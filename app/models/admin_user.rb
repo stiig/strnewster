e472305c27548f9191130ba1aa0eeded860cb0a0
stiig <vasiliyrps2@gmail.com>
2017-02-23 14:31:28 +0500

    add active admin

---
+++ app/models/admin_user.rb
@@ -0,0 +1,30 @@
+class AdminUser < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, 
+         :recoverable, :rememberable, :trackable, :validatable
+end
+
+# == Schema Information
+#
+# Table name: admin_users
+#
+#  created_at             :datetime         not null
+#  current_sign_in_at     :datetime
+#  current_sign_in_ip     :inet
+#  email                  :string           default(""), not null
+#  encrypted_password     :string           default(""), not null
+#  id                     :integer          not null, primary key
+#  last_sign_in_at        :datetime
+#  last_sign_in_ip        :inet
+#  remember_created_at    :datetime
+#  reset_password_sent_at :datetime
+#  reset_password_token   :string
+#  sign_in_count          :integer          default(0), not null
+#  updated_at             :datetime         not null
+#
+# Indexes
+#
+#  index_admin_users_on_email                 (email) UNIQUE
+#  index_admin_users_on_reset_password_token  (reset_password_token) UNIQUE
+#
