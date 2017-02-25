25b3d3d9514bb41fb57542d1c6c9f5b0c517fbe1
stiig <vasiliyrps2@gmail.com>
2017-02-26 02:33:19 +0500

    fixes by rubocop

--- app/models/admin_user.rb
+++ app/models/admin_user.rb
@@ -1,7 +1,9 @@
+# frozen_string_literal: true
+
 class AdminUser < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, 
+  devise :database_authenticatable,
          :recoverable, :rememberable, :trackable, :validatable
 end
 
