e9a6b7ebbd3ab5cd93bd53259f7ed230d0474d0e
stiig <vasiliyrps2@gmail.com>
2017-03-01 12:41:45 +0500

    add navbar and remove active_admin.js into vendor

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,10 +10,9 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-// require jquery
 // require jquery_ujs
-// require bootstrap
+//= require jquery
+//= require bootstrap
 //= require turbolinks
 //= require_tree .
-//= stub active_admin
 
