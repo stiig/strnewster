1b58db31d5f4c7788cb4b5f37cbee9d23dec7263
stiig <vasiliyrps2@gmail.com>
2017-03-08 13:15:44 +0500

    add simple filter for all news

--- app/controllers/news_controller.rb
+++ app/controllers/news_controller.rb
@@ -2,6 +2,7 @@
 
 class NewsController < ApplicationController
   def index
-    @parsed_articles = ParsedArticle.includes(:feed_source).order(date: :desc).page(params[:page]).per(12)
+    @q = ParsedArticle.ransack(params[:q])
+    @parsed_articles = @q.result.includes(:feed_source).order(date: :desc).page(params[:page]).per(12)
   end
 end
