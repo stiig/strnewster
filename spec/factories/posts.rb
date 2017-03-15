83b757831ca8a2702877fcd3c2ff587f2012fc7b
stiig <vasiliyrps2@gmail.com>
2017-03-15 10:58:45 +0500

    add some tests

--- spec/factories/posts.rb
+++ spec/factories/posts.rb
@@ -3,7 +3,6 @@
 FactoryGirl.define do
   factory :post do
     text { Faker::Lorem.paragraph }
-    status { [0, 1].sample }
     last_parsed_article_id { Faker::Number.number(3) }
   end
 end
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -4,6 +4,12 @@ describe Post do
   subject { create(:post) }
 
   it 'can be created' do
-    is_expected.to be_truthy
+    is_expected.to be
+  end
+
+  it 'must have status created by default' do
+    is_expected.to respond_to(:status, :created?, :published?, :created!, :published!)
+    expect(subject.created?).to be_truthy
+    expect(subject.published?).to be_falsey
   end
 end
