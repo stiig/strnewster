74ff20fb6fd5b8189c090b0e99c3ff7066d218e0
stiig <vasiliyrps2@gmail.com>
2017-03-19 12:58:56 +0500

    refactor parser runner servise

--- app/services/parser_runner_service.rb
+++ app/services/parser_runner_service.rb
@@ -1,28 +1,43 @@
 # frozen_string_literal: true
 
-class ParserRunnerService
-  def self.parse_data
-    logger = Rails.logger
-    logger.info 'ParserRunnerService start'
-    FeedSource.activated.each do |element|
-      parsed_elements = []
-      begin
-        logger.info "perform #{element.title}: #{element.url}"
-        parsed_elements = FeedParser.new(element.url).perform
-      rescue Net::OpenTimeout => e
-        logger.warn "#{element.title} temporary not allowed by time-out. Details: #{e.message}"
-      rescue SocketError => e
-        logger.warn "#{element.title} temporary not allowed. Details: #{e.message}"
-      end
-      if parsed_elements.blank?
-        logger.warn "#{element.title}: #{element.url} empty answer"
-      else
-        parsed_elements.each do |obj|
-          parsed_item = obj.to_h.merge(feed_source: element)
-          ParsedArticle.find_or_create_by(parsed_item)
+module ParserRunnerService
+  module_function
+
+  def parse_data
+    with_log do |logger|
+      FeedSource.activated.each do |element|
+        parsed_elements = []
+        begin
+          logger.info "perform #{element.title}: #{element.url}"
+          parsed_elements = FeedParser.new(element.url).perform
+        rescue Net::OpenTimeout => e
+          logger.warn "#{element.title} temporary not allowed by time-out. Details: #{e.message}"
+        rescue SocketError => e
+          logger.warn "#{element.title} temporary not allowed. Details: #{e.message}"
         end
+
+        handle_parsed_elements(parsed_elements, element, logger)
       end
     end
+  end
+
+  private_class_method
+
+  def with_log
+    logger = Rails.logger
+    logger.info 'ParserRunnerService start'
+    yield logger
     logger.info 'ParserRunnerService finish'
+  end
+
+  def handle_parsed_elements(elements, parent_source, logger)
+    if elements.blank?
+      logger.warn "#{parent_source.title}: #{parent_source.url} empty answer"
+    else
+      elements.each do |obj|
+        parsed_item = obj.to_h.merge(feed_source: parent_source)
+        ParsedArticle.find_or_create_by(parsed_item)
+      end
+    end
   end
 end
