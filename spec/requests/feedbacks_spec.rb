889f488d05d5f0ade0c189a85ac4381465fb15d9
stiig <vasiliyrps2@gmail.com>
2017-03-30 11:23:34 +0500

    add test for flash_messages helper

--- spec/requests/feedbacks_spec.rb
+++ spec/requests/feedbacks_spec.rb
@@ -7,4 +7,12 @@ describe 'Feedbacks' do
       expect(response).to have_http_status(200)
     end
   end
+
+  describe 'POST /feedback' do
+    it 'create new feedback' do
+      post feedbacks_path, params: { feedback: attributes_for(:feedback) }
+      follow_redirect!
+      expect(response.body).to match(/alert alert-success/)
+    end
+  end
 end
