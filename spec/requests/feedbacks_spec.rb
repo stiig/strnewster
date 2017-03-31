63e64f8193d43030c2b823ca055e6929d8ab70b2
stiig <vasiliyrps2@gmail.com>
2017-03-31 12:07:57 +0500

    add form_errors helper spec

--- spec/requests/feedbacks_spec.rb
+++ spec/requests/feedbacks_spec.rb
@@ -9,10 +9,21 @@ describe 'Feedbacks' do
   end
 
   describe 'POST /feedback' do
-    it 'create new feedback' do
-      post feedbacks_path, params: { feedback: attributes_for(:feedback) }
-      follow_redirect!
-      expect(response.body).to match(/alert alert-success/)
+    context 'with valid params' do
+      it 'create a new feedback' do
+        post feedbacks_path, params: { feedback: attributes_for(:feedback) }
+        follow_redirect!
+        expect(response.body).to match(/alert alert-success/)
+      end
+    end
+
+    context 'with invalid params' do
+      it "re-renders the 'new' template" do
+        post feedbacks_path, params: { feedback: { invalid: :data } }
+        expect(response).not_to redirect_to(root_path)
+        expect(response.body).to match(/error_explanation/)
+        expect(response.body).to match(/Исправьте следующие ошибки перед сохранением:/)
+      end
     end
   end
 end
