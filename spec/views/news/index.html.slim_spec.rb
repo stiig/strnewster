e3229340b7f465db3cdc72f69c02326681811bc9
stiig <vasiliyrps2@gmail.com>
2017-04-04 12:02:31 +0500

    add test for news index view

--- spec/views/news/index.html.slim_spec.rb
+++ spec/views/news/index.html.slim_spec.rb
@@ -1,5 +1,10 @@
 # frozen_string_literal: true
 
 describe 'news/index.html.slim' do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'should render list of parsed articles' do
+    assign(:parsed_articles, create_list(:parsed_article, 20))
+    stub_template 'shared/_paginator' => 'stubbed paginator'
+    stub_template '_search_form' => 'stubbed paginator'
+    render
+  end
 end
