85072e45726c0a56cf9d37cbcbd9e5c84ce76393
stiig <vasiliyrps2@gmail.com>
2017-04-05 11:56:59 +0500

    remove all pending tests

--- spec/views/publics/index.html.slim_spec.rb
+++ spec/views/publics/index.html.slim_spec.rb
@@ -1,5 +1,8 @@
 # frozen_string_literal: true
 
 describe 'publics/index.html.slim' do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'should render list of publics' do
+    assign(:publics, create_list(:public, 20))
+    render
+  end
 end
