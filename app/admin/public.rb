9634b917f836beade1a8eada02f42a9df8c729da
stiig <vasiliyrps2@gmail.com>
2017-04-11 11:54:35 +0500

    add form for public

--- app/admin/public.rb
+++ app/admin/public.rb
@@ -1,16 +1,17 @@
 # frozen_string_literal: true
 
 ActiveAdmin.register Public do
-  # See permitted parameters documentation:
-  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-  #
-  # permit_params :list, :of, :attributes, :on, :model
-  #
-  # or
-  #
-  # permit_params do
-  #   permitted = [:permitted, :attributes]
-  #   permitted << :other if params[:action] == 'create' && current_user.admin?
-  #   permitted
-  # end
+  permit_params :title, :outer_id, :mode, :wide, :width, :height
+
+  form do |_f|
+    inputs do
+      input :title
+      input :outer_id
+      input :mode
+      input :wide
+      input :width
+      input :height
+    end
+    actions
+  end
 end
