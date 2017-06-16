df855c6822c3b32d1696cab7135e67571e6f061c
stiig <vasiliyrps2@gmail.com>
2017-06-16 12:27:37 +0500

    create blank image

--- app/services/image_creator_service.rb
+++ app/services/image_creator_service.rb
@@ -3,7 +3,11 @@
 module ImageCreatorService
   class << self
     def generate_image(text)
-
+      MiniMagick::Tool::Convert.new do |image|
+        image.size '1200x1200'
+        image.caption text
+        image << 'blank.png'
+      end
     end
     private
   end
