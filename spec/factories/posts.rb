8a7cbc5d039a1caace1b0a0a5b8171a8243b2a0b
stiig <vasiliyrps2@gmail.com>
2017-11-06 15:28:06 +0500

    rename factory girl

--- spec/factories/posts.rb
+++ spec/factories/posts.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-FactoryGirl.define do
+FactoryBot.define do
   factory :post do
     text { Faker::Lorem.paragraph }
     last_parsed_article_id { Faker::Number.number(3) }
